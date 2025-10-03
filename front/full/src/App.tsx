3bd1d1bdb06361add70e51c0e308c475f4835eef
RafaelRosaMD <rafael@MacBook-Air-de-Rafael.local>
2025-10-03 19:14:01 -0300

    segue swagger off 2

--- front/full/src/App.tsx
+++ front/full/src/App.tsx
@@ -2,7 +2,6 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import LoginPage from "./pages/LoginPage";
-import OrdensPage from "./pages/OrdensPage";
 import OrdensList from "./pages/OrdensList";
 import OrdemForm from "./pages/OrdemForm";
 
