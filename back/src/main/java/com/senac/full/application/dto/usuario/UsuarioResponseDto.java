431a20bd1b177df40aae6d7ec7c588091ebee355
RafaelRosaMD <rafael@MacBook-Air-de-Rafael.local>
2025-10-09 21:16:07 -0300

    primeiro novotempo 2

--- back/src/main/java/com/senac/full/application/dto/usuario/UsuarioResponseDto.java
+++ back/src/main/java/com/senac/full/application/dto/usuario/UsuarioResponseDto.java
@@ -1,5 +1,11 @@
 package com.senac.full.application.dto.usuario;
 
+import com.senac.full.domain.entities.Usuario;
+
 public record UsuarioResponseDto (Long id, String nome, String cpf, String senha, String email, String role) {
+
+    public UsuarioResponseDto(Usuario usuario) {
+        this(id, nome, cpf, senha, email, "ROLE_USER");
+    }
 
 }
